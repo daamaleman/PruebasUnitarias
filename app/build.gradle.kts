d8d42c1760acfe23dab361aa197732a8115a6ce1
daamaleman <diedereicha@uamv.edu.ni>
2026-06-08 19:15:26 -0600

    fix: actualizar compileSdk y targetSdk a 37 para compatibilidad con dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "ni.edu.uam.pruebasunitarias"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "ni.edu.uam.pruebasunitarias"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
